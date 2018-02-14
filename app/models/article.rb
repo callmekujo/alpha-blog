7e1d1179dbf40305b633ae2a223d9b5a24460d68
Jordan Bodily <callmekujo@gmail.com>
2018-02-14 09:21:57 +0000

    adding validation to title and description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord #rails 4 is Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }  #make sure theere is a title on the article before being saved to the DB
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 
 end
