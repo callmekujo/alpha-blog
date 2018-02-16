456cc55f0b5d8d6e8ba5effd42ec2d09367d225b
Jordan Bodily <callmekujo@gmail.com>
2018-02-16 06:46:23 +0000

    adding new links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   
   def new 
     @article = Article.new
@@ -17,9 +20,16 @@ class ArticlesController < ApplicationController
     else 
       render 'new'
     end
-   # render plain: params[:article].inspect
-    #@article.save
-    #redirect_to article_path(@article)
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else 
+      render 'edit'
+    end
   end
   
   def show
